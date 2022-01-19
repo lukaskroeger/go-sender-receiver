043c8682070cd8a0a76b2d56a2c68eb52b4fe6b9
Lukas Kröger <goofie254@gmx.de>
2022-01-19 16:35:17 +0100

    acks, nacks and retransmits

--- receiver.go
+++ receiver.go
@@ -9,9 +9,28 @@ import (
 )
 
 type message struct {
-	SeqNum int
-	Time   time.Time
+	SlidingWindow int
+	SeqNum        int
+	Time          time.Time
 }
+type ack struct {
+	SlidingWindow int
+	IsNak         bool
+}
+
+const slidingWindowSize = 10000
+
+var lastTime time.Time
+var transferedData int
+var transferedTime time.Duration
+var latencySum float64
+var transfaredPackages int
+var mode string
+var lostPackages int
+
+var missingPackages []int
+
+var tempRetransmitts int
 
 func main() {
 	arguments := os.Args
@@ -21,56 +40,106 @@ func main() {
 	}
 
 	port := ":" + arguments[2]
-	mode := arguments[1]
+	mode = arguments[1]
 	fmt.Printf("Mode: %s \n", mode)
 
 	addr, err := net.ResolveUDPAddr("udp4", port)
 	checkError(err)
 
-	con, err := net.ListenUDP("udp4", addr)
+	conRec, err := net.ListenUDP("udp4", addr)
 	checkError(err)
+	defer conRec.Close()
 
-	defer con.Close()
+	lastTime = time.Now()
+	transferedData = 0
+	latencySum = float64(0)
+	transfaredPackages = 0
+	lostPackages = 0
 
-	buffer := make([]byte, 4096)
+	buffer := make([]byte, 1024)
 	lastPackTime := time.Now()
 	lastPackNum := 0
-	lostPackages := 0
-	lastTime := time.Now()
-	transferedData := 0
-	var transferedTime time.Duration
-	latencySum := float64(0)
-	transfaredPackages := 0
 	data := new(message)
+	missingPackages = make([]int, 0)
+	tempRetransmitts = 0
+
 	for {
-		n, _, _ := con.ReadFromUDP(buffer)
+		n, conSendAddr, _ := conRec.ReadFromUDP(buffer)
 		transferedTime += time.Since(lastPackTime)
 		transferedData += 8 * n
 		transfaredPackages += 1
 		if mode == "complete" {
 			json.Unmarshal(buffer[:n], data)
-			if (data.SeqNum - lastPackNum) > 1 {
-				lostPackages += data.SeqNum - lastPackNum - 1
+			if removeFromOrderedIfThere(data.SlidingWindow, missingPackages) {
+				sendAcknowledgement(conSendAddr, data.SlidingWindow)
+				tempRetransmitts += 1
+			} else {
+				diff := (data.SlidingWindow - lastPackNum) % slidingWindowSize
+				if diff > 1 {
+					lostPackages += diff
+					sendNotAck(conSendAddr, lastPackNum+1, diff)
+					for n := lastPackNum + 1; n < lastPackNum+diff; n++ {
+						missingPackages = append(missingPackages, n%slidingWindowSize)
+					}
+				} else {
+					sendAcknowledgement(conSendAddr, data.SlidingWindow)
+				}
 			}
 			latencySum += time.Since(data.Time).Seconds()
-			lastPackNum = data.SeqNum
-		}
-		if time.Since(lastTime).Seconds() > 1 {
+			lastPackNum = data.SlidingWindow
 
-			if mode == "complete" {
-				complexOutput(transfaredPackages, transferedData, transferedTime, data.SeqNum, latencySum, lostPackages)
-			} else {
-				simpleOutput(transfaredPackages, transferedData, transferedTime)
-			}
-			lastTime = time.Now()
-			transferedData = 0
-			transferedTime = time.Duration(0)
-			latencySum = float64(0)
-			transfaredPackages = 0
 		}
+		output(data)
 		lastPackTime = time.Now()
 	}
 
+}
+
+func sendAcknowledgement(addr *net.UDPAddr, seqNumber int) {
+	addrSend, err := net.ResolveUDPAddr("udp4", addr.IP.String()+":4445")
+	checkError(err)
+
+	con, err := net.DialUDP("udp4", nil, addrSend)
+	checkError(err)
+	defer con.Close()
+
+	ackPackage := ack{IsNak: false}
+	if len(missingPackages) > 0 {
+		ackPackage.SlidingWindow = (missingPackages[0] - 1) % slidingWindowSize
+	} else {
+		ackPackage.SlidingWindow = seqNumber
+	}
+	data, _ := json.Marshal(ackPackage)
+	con.Write(data)
+}
+
+func sendNotAck(addr *net.UDPAddr, from int, count int) {
+	addrSend, err := net.ResolveUDPAddr("udp4", addr.IP.String()+":4445")
+	checkError(err)
+
+	con, err := net.DialUDP("udp4", nil, addrSend)
+	checkError(err)
+	defer con.Close()
+	for num := from; num <= from+count; num++ {
+		data, err := json.Marshal(ack{num % slidingWindowSize, true})
+		checkError(err)
+		con.Write(data)
+	}
+}
+
+func output(data *message) {
+	if time.Since(lastTime).Seconds() > 1 {
+		if mode == "complete" {
+			complexOutput(transfaredPackages, transferedData, transferedTime, data.SeqNum, latencySum, lostPackages)
+		} else {
+			simpleOutput(transfaredPackages, transferedData, transferedTime)
+		}
+		lastTime = time.Now()
+		transferedData = 0
+		transferedTime = time.Duration(0)
+		latencySum = float64(0)
+		transfaredPackages = 0
+	}
 }
 
 func simpleOutput(transferedPackages int, transferedData int, transferedTime time.Duration) {
@@ -86,7 +155,23 @@ func complexOutput(transferedPackages int, transferedData int, transferedTime ti
 	fmt.Printf("Datarate : %f Mbit/s\n", float64(transferedData)/transferedTime.Seconds()/1024/1024)
 	fmt.Printf("Latency: %f \n", latencySum/float64(transferedPackages))
 	fmt.Printf("Lost packages: %d \n", lostPackages)
-	fmt.Print("\033[A\033[A\033[A\033[A\033[A")
+	fmt.Printf("Currenly missing: %d \n", len(missingPackages))
+	fmt.Printf("Currenly missing: %d \n", tempRetransmitts)
+	fmt.Print("\033[A\033[A\033[A\033[A\033[A\033[A\033[A")
+}
+
+func removeFromOrderedIfThere(value int, list []int) bool {
+	for i := 0; i < len(list); i++ {
+		if list[i] == value {
+			list = append(list[:i], list[i+1:]...)
+			return true
+		}
+		if list[i] > value {
+			return false
+		}
+
+	}
+	return false
 }
 
 func checkError(err error) {
--- sender.go
+++ sender.go
@@ -9,9 +9,27 @@ import (
 )
 
 type message struct {
-	SeqNum int
-	Time   time.Time
+	SlidingWindow int
+	SeqNum        int
+	Time          time.Time
 }
+
+type ack struct {
+	SlidingWindow int
+	IsNak         bool
+}
+
+const slidingWindowSize = 10000
+
+var slidingWindow [slidingWindowSize]message
+var slidingWindowIndex int
+
+var ackBuff []byte
+
+var conSend *net.UDPConn
+var conRec *net.UDPConn
+
+var timerMap map[int]*time.Timer
 
 func main() {
 	arguments := os.Args
@@ -21,27 +39,77 @@ func main() {
 	}
 
 	destination := arguments[1]
-	addr, err := net.ResolveUDPAddr("udp4", destination)
+	addrSend, err := net.ResolveUDPAddr("udp4", destination)
 	checkError(err)
 
-	con, err := net.DialUDP("udp4", nil, addr)
+	addrRec, err := net.ResolveUDPAddr("udp4", ":4445")
 	checkError(err)
 
-	defer con.Close()
+	conSend, err = net.DialUDP("udp4", nil, addrSend)
+	checkError(err)
+	defer conSend.Close()
+
+	conRec, err = net.ListenUDP("udp4", addrRec)
+	checkError(err)
+	defer conRec.Close()
+
+	ackBuff = make([]byte, 1024)
+	timerMap = make(map[int]*time.Timer)
+
+	slidingWindowIndex = 0
 	seqNumber := 0
+	until := 0
 	for {
-		data, _ := json.Marshal(message{seqNumber, time.Now()})
-		con.Write(data)
-		for !isAcknowledged(seqNumber) {
-			con.Write(data)
+		if seqNumber >= slidingWindowSize {
+			until = (packageAcknowleged(conRec) + 1) % slidingWindowSize
 		}
-		seqNumber += 1
+		for slidingWindowIndex != until || seqNumber < slidingWindowSize {
+			message := message{slidingWindowIndex, seqNumber, time.Now()}
+			data, _ := json.Marshal(message)
+			slidingWindow[slidingWindowIndex] = message
+			conSend.Write(data)
+
+			fmt.Println("Transmitt: ", slidingWindowIndex)
+			time.Sleep(time.Second * 1)
+			slidingWindowIndex = (slidingWindowIndex + 1) % slidingWindowSize
+			seqNumber += 1
+		}
+
 	}
 
 }
 
-func isAcknowledged(seqNumber int) bool {
-	return true
+func packageAcknowleged(con *net.UDPConn) int {
+	for true {
+		data := new(ack)
+		n, _, err := con.ReadFromUDP(ackBuff)
+		if err != nil {
+			fmt.Println(err)
+		}
+		json.Unmarshal(ackBuff[:n], data)
+		result := data.SlidingWindow
+		if data.IsNak {
+			retransmittPackage(data.SlidingWindow)
+		} else {
+			fmt.Println("Ack: ", data.SlidingWindow)
+			if timerMap[data.SlidingWindow] != nil {
+				timerMap[data.SlidingWindow].Stop()
+			}
+			return result
+		}
+
+	}
+	return -1
+}
+
+func retransmittPackage(num int) {
+	message := slidingWindow[num]
+	message.Time = time.Now()
+	data, _ := json.Marshal(message)
+	conSend.Write(data)
+	fmt.Println("Retransmitt: ", num)
+	f := func() { retransmittPackage(num) }
+	timerMap[num] = time.AfterFunc(time.Second*10, f)
 }
 
 func checkError(err error) {
